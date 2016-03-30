9df7c41ec1cdd0e53e2f457bf732c1ca4566e13e
HuiminJian <jianhuimin89@gmail.com>
2016-03-29 18:00:42 -0700

    war generated

--- src/main/java/com/sjsu/cmpe275/lab2/RestAssign273Application.java
+++ src/main/java/com/sjsu/cmpe275/lab2/RestAssign273Application.java
@@ -2,14 +2,22 @@ package com.sjsu.cmpe275.lab2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class RestAssign273Application {
-
+public class RestAssign273Application extends SpringBootServletInitializer {
+										//generate war
 	public static void main(String[] args) {
 
 		SpringApplication.run(RestAssign273Application.class, args);
+	}
+
+	//generate war
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestAssign273Application.class);
 	}
 }
